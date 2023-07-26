838f6946f15bc86779f0faa554308ef3dd1d0d17
HAdriney <hadrineytaylors@gmail.com>
2023-07-26 15:50:00 -0300

    add libs

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,63 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const path = require('path');
+const webpack = require('webpack');
 
 
-export default {
-  entry: './src/index.js', // Arquivo principal da aplicação
+module.exports = {
+  mode: 'production', // Ou 'development' se desejar ver os detalhes dos arquivos gerados
+  entry: './src/server.ts', // Arquivo principal da aplicação
   output: {
-    path: path.resolve(__dirname, 'dist'), // Caminho de saída dos arquivos transpilados
+    path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js', // Nome do arquivo de saída
   },
+plugins: [
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^child_process$/,
+    }),
+    new webpack.IgnorePlugin({
+      resourceRegExp: /^node-gyp$/,
+    }),
+  ],
   resolve: {
-    extensions: ['.js', '.jsx'], // Suportar arquivos JavaScript e JSX
+    extensions: ['.ts', '.js'],
+    fallback: {
+      path: require.resolve('path-browserify'),
+      crypto: false,
+      fs: false,
+      assert: require.resolve('assert/'),
+      process: require.resolve('process/browser'),
+      stream: require.resolve('stream-browserify'),
+      buffer: require.resolve('buffer/'),
+      util: require.resolve('util/'),
+      querystring: require.resolve("querystring-es3"),
+      zlib: require.resolve('browserify-zlib'),
+      net: require.resolve('net'),
+      tls: require.resolve('tls'),
+      http: require.resolve("stream-http"),
+      os: require.resolve("os-browserify/browser"),
+      constants: require.resolve('constants-browserify'),
+      crypto: require.resolve('crypto-browserify'),
+      buffer: require.resolve('buffer/'),
+      util: require.resolve('util/'),
+      querystring: require.resolve('querystring-es3'),
+      zlib: require.resolve('browserify-zlib'),
+      nock: false,
+    },
   },
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/, // Aplicar a regra para arquivos .js e .jsx
+        test: /\.html$/,
+        use: 'html-loader',
+      },
+      {
+        test: /\.ts$/, // Aplicar a regra para arquivos .ts
+        use: 'ts-loader', // Usar o ts-loader para transpilar os arquivos TypeScript
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader', // Usar o babel-loader para transpilar os arquivos JavaScript
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'], // Presets do Babel para suportar JavaScript moderno e React
-          },
-        },
+      },
+      {
+        test: /\.js$/, // Aplicar a regra para arquivos .js
+        use: 'babel-loader', // Usar o babel-loader para transpilar os arquivos JavaScript
+        exclude: /node_modules/,
       },
     ],
   },
